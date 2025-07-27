938ab708e2708eefb6e740d9a8c97551b05783db
user.email <mrdilmurat777@gmail.com>
2025-07-27 21:44:06 +0600

    feat: add Navbar, Hero, Program, About, Compus and Testimonials components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-
+import About from './components/About/About';
+import Compus from './components/Compus/Compus';
+import Hero from './components/Hero/Hero';
+import Navbar from './components/Navbar/Navbar';
+import Programs from './components/Programs/Programs';
+import Testimonials from './components/Testimonials/Testimonials';
+import Title from './components/Title/Title';
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar />
+      <Hero />
+      <div className="container">
+        <Title subTitle="Our PROGRAM" title="What We Offer" />
+        <Programs />
+        <About />
+        <Title subTitle="Gallery" title="Compus Photos" />
+        <Compus />
+        <Title subTitle="TESTIMONIALS" title="What Students Says" />
+        <Testimonials />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
